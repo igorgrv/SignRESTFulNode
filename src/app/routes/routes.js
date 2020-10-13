38b575b98a6a0a7b0dcef8bf7e5575110d2429ce
Vilela <867695@cognizant.com>
2020-10-13 00:30:52 -0300

    adicionado swagger

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -3,9 +3,12 @@ const usuarioRoutes = UsuarioController.routes();
 const usuarioController = new UsuarioController();
 const auth = require('../middleware/auth');
 
-module.exports = (app) => {
-	app.post(usuarioRoutes.signup, usuarioController.signup());
-	app.post(usuarioRoutes.signin, usuarioController.signin());
+const initializeEndPoints = (app) => {
+
+  app.post(usuarioRoutes.signup, usuarioController.signup());
+  app.post(usuarioRoutes.signin, usuarioController.signin());
   app.get(usuarioRoutes.usuarios, auth, usuarioController.usuarios());
   app.get(usuarioRoutes.usuario, auth, usuarioController.usuario());
 };
+
+module.exports = initializeEndPoints;
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
+const swaggerDoc = require('./swaggerDoc');
+const routes = require('../app/routes/routes');
 const health = require('express-ping');
-app.use(health.ping('/health'));
-// const swaggerJsDoc = require('swagger-jsdoc')
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(health.ping('/health'));
 app.use(express.json());
 
-const routes = require('../app/routes/routes');
 routes(app);
+swaggerDoc(app);
 
 module.exports = app;
